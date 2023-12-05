7fb709f25ddbe1303b77a3e79964d0c73cd0ed62
gfredex <g.fredex@gmail.com>
2023-12-05 14:54:04 +0300

    feat: add solution Fibonacci number located at the index position

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -259,8 +259,18 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  let a = 1;
+  let b = 1;
+  if (index) {
+    for (let i = 3; i <= index; i += 1) {
+      const c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
+  return 0;
 }
 
 /**
